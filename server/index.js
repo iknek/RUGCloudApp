ea21648a9dd85591008475cc4785545311c90dbb
LeoBtk <leonard.benthake@tum.de>
2023-09-11 12:42:13 +0200

    add new book apis

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { connectDB } = require("./db/connect.js");
 const { getItem, getAllItems, createItem } = require("./db/items.js");
+const { getBook, getAllBooks, createBook } = require("./db/books.js");
 const bodyParser = require("body-parser"); // Require the body-parser middleware
 
 const PORT = process.env.PORT || 3001;
@@ -30,6 +31,25 @@ app.post("/item", async (req, res) => {
   const item = await createItem(req.body);
   res.json(item);
 });
+
+app.get("/books", async (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  const items = await getAllBooks();
+  res.json(items);
+});
+app.get("/book/:bookID", async (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  const { itemID } = req.params;  
+  const item = await getBook(bookID);
+  res.json(item);
+});
+app.post("/book", async (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  const item = await createBook(req.body);
+  res.json(item);
+});
+
+
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
