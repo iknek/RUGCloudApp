80f6f2027a9d6ea529ef393a148e7334e258d2e9
iknek <imad.alihodzic@yahoo.com>
2023-09-15 14:05:18 +0200

    updated env template and bash script for auto-env gen

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,10 +1,11 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
+require('dotenv').config({ path: '../../.env' });
 
 module.exports = function(app) {
   app.use(
     ['/api', '/items', '/item', '/books', '/book'],
     createProxyMiddleware({
-      target: 'http://server:3001',
+      target: `http://server:${process.env.WORKER_PORT}`,
       changeOrigin: true,
     })
   );
