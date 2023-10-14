80a812586bca06f70fd199acb08b97e2b3fc429d
iknek <imad.alihodzic@yahoo.com>
2023-10-14 12:37:49 +0200

    terraform stuffsies

--- Terraform/instance.tf
+++ Terraform/instance.tf
@@ -3,11 +3,24 @@ resource "openstack_compute_instance_v2" "flatcar" {
     name = "Flatcar VM"
     image_id = openstack_images_image_v2.flatcar.id
     flavor_name = var.flavor_name
-    security_groups = ["default"]
+    security_groups = ["default", openstack_networking_secgroup_v2.basic.name]
     
     user_data = data.ct_config.flatcar.rendered
 
     network {
         uuid = openstack_networking_network_v2.internal.id
     }
+}
+
+resource "openstack_compute_instance_v2" "worker" {
+  name = "Worker VM"
+  image_id = openstack_images_image_v2.flatcar.id
+  flavor_name = var.flavor_name
+  security_groups = ["default", openstack_networking_secgroup_v2.basic.name]
+
+  user_data = data.ct_config.worker.rendered
+
+  network {
+    uuid = openstack_networking_network_v2.internal.id
+  }
 }
